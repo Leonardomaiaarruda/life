a2b622ad0a7e3e898e5e94084e8d2e983d90833f
Leonardo Maia <76956348+Leonardomaiaarruda@users.noreply.github.com>
2026-09-10 20:00:40 -0300

    Atualiza versão para V20 Bloco 3

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ window.CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyZzoAEi14K4ItWRW8tohjFh1e25OfHu33CENlIio0sQbvuu0E6SQzOZk5gbF6FhbPB/exec",
   APP_NAME: "MetaLife",
   DEMO_MODE: true,
-  VERSION: "20.1.0"
+  VERSION: "20.3.0"
 };
