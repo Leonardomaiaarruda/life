f1b8f892839a4a55910eed71b91384869dfdbee4
Leonardo Maia <76956348+Leonardomaiaarruda@users.noreply.github.com>
2026-09-10 16:53:14 -0300

    Atualiza MetaLife para versão 16.0.0

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ window.CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyZzoAEi14K4ItWRW8tohjFh1e25OfHu33CENlIio0sQbvuu0E6SQzOZk5gbF6FhbPB/exec",
   APP_NAME: "MetaLife",
   DEMO_MODE: true,
-  VERSION: "14.0.0"
+  VERSION: "16.0.0"
 };
