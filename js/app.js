bab293935457b2797012aa3a8b26ec0270954524
Leonardomaiaarruda <leonardomaiaarruda@gmail.com>
2026-09-10 00:21:12 -0300

    Corrige identificacao da sessao em desafios

--- js/app.js
+++ js/app.js
@@ -3587,7 +3587,7 @@ function challengeUserName() {
   return state.user?.name || state.user?.nome || "Você";
 }
 
-const CHALLENGE_BACKEND_BUILD = "2026.09.10-session-2";
+const CHALLENGE_BACKEND_BUILD = "2026.09.10-session-3";
 
 async function challengeSessionMessage(error = "") {
   try {
