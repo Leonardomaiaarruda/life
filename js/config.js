95f395d1db4b7b9813a7c7fc7b537f7ce9ca5227
Leonardo Maia <76956348+Leonardomaiaarruda@users.noreply.github.com>
2026-09-11 11:53:58 -0300

    Atualiza versão para Desafios 2.0

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ window.CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyZzoAEi14K4ItWRW8tohjFh1e25OfHu33CENlIio0sQbvuu0E6SQzOZk5gbF6FhbPB/exec",
   APP_NAME: "MetaLife",
   DEMO_MODE: true,
-  VERSION: "21.2.3"
+  VERSION: "22.0.0"
 };
