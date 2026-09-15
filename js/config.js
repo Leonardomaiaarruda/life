34f4d3d78181abe140b59a4960de305785682b87
Leonardo Maia <76956348+Leonardomaiaarruda@users.noreply.github.com>
2026-09-15 13:04:55 -0300

    Atualiza MetaLife para 22.3.1

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyZzoAEi14K4ItWRW8tohjFh1e25OfHu33CENlIio0sQbvuu0E6SQzOZk5gbF6FhbPB/exec",
   APP_NAME: "MetaLife",
   DEMO_MODE: true,
-  VERSION: "22.3.0",
+  VERSION: "22.3.1",
 
   // O Apps Script/Sheets continua sendo o backend oficial do MetaLife.
   DATA_PROVIDER: "apps-script",
